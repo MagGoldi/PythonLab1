458e42caac02998442d864f67e9a9ebab231bb1d
MagGoldi <StivenH0king@yandex.ru>
2022-10-22 10:55:05 +0400

    rework

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 
 from bs4 import BeautifulSoup
 import requests
-
+import datetime
 
 def get_article(url):
     html_page = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
@@ -19,13 +19,13 @@ def get_URL(month, year):
     return url
 
 
-def get_data(article):
+def get_data(article, year, mounth):
     date = article.find("td", class_="first").text
     temp = article.find("td", class_="first_in_group").text
     wind = article.find("span").text
     pressure = list(article.find_all("td"))
     pressure = str(pressure[2].text)
-    data = date + ") " + "Day:" + temp + ", " + wind + ", " + pressure
+    data = str(datetime.date(int(year), int(mounth), int(date))) +"; " + "Day:" + temp + ", " + wind + ", " + pressure
     return data
 
 
@@ -39,15 +39,8 @@ def get_data_evening(article):
 
 
 def write_day(data, path_to_csv=os.path.join("PYTHON", "dataset.csv")):
-    file_name = open(path_to_csv, "a", encoding="utf-8")
+    file_name = open("dataset.csv", "a", encoding="utf-8")
     file_name.write(data)
-    file_name.write("\n")
-
-
-def write_month(months, month, year, path_to_csv=os.path.join("PYTHON", "dataset.csv")):
-    file_name = open(path_to_csv, "a", encoding="utf-8")
-    file_name.write(months[month])
-    file_name.write(str(year))
     file_name.write("\n")
 
 
@@ -64,17 +57,17 @@ def get_time():
 
 
 def run(path_to_csv=os.path.join("PYTHON", "dataset.csv")):
-    file_name = open(path_to_csv, "w", encoding="utf-8")
+    #make_dir()
+    file_name = open("dataset.csv", "w", encoding="utf-8")
     months = ["", "January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December"]
+              "July", "August", "September", "October", "November", "December"]       
     year = 2022
     month = 9
-    while year != 2007:
+    while year != 2008:
         while month != 0:
             article = get_article(get_URL(month, year))
-            write_month(months, month, year)
             for i in range(2, len(article) - 1):
-                write_day(get_data(article[i]) + get_data_evening(article[i]))
+                write_day(get_data(article[i], year, month) + get_data_evening(article[i]))
             print("Parsing of", months[month], year)
             get_time()
             month -= 1
